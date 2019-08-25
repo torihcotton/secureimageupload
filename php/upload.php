b866ead5cf3c3cb5726b3efd81403890706bab98
torihcotton <54193943+torihcotton@users.noreply.github.com>
2019-08-25 15:36:00 -0500

    Update upload.php

--- php/upload.php
+++ php/upload.php
@@ -2,12 +2,20 @@
 $uploaddir = 'uploads/';
 
 $uploadfile = $uploaddir . basename($_FILES['image']['name']);
+      $file_tmp = $_FILES['image']['tmp_name'];
+      $file_type = $_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+    
+    $extensions= array("jpeg","jpg","png");
 
-if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
-    echo "Your image has been uploaded.";
-} else {
+ if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
+    echo "Upload successful.";
+    }else{
     echo "Upload failed.";
 }
 ?> 
-
 
